4b1d0f1213d93021f7af985a285a30ad7f0266fb
Nikos Koukis <nikolaos@slamcore.com>
2019-05-21 14:23:07 +0100

    Add to install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -1,10 +1,4 @@
-if [[  $# != 2 ]]; then
-    name="${BASH_SOURCE[*]}"
-    echo "Usage: $name <ros-version-to-install> <desktop-full|desktop|ros-base>"
-    exit 1
-fi
-
-ROS_DISTRO="${1:-kinetic}"
+ROS_DISTRO="${1:-melodic}"
 INSTALLATION_TYPE="${2:-desktop-full}"
 
 # Sources list, apt-key
@@ -14,14 +8,32 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB0
 sudo apt-get update
 
 # Install ROS packages
-sudo apt-get install "ros-${ROS_DISTRO}-${INSTALLATION_TYPE}"
+sudo apt-get install -y "ros-${ROS_DISTRO}-${INSTALLATION_TYPE}"
 
 # Initialize rosdep
 sudo rosdep init
 rosdep update
 
 # Source config file
-source /opt/ros/$(ROS_DISTRO)/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 
 # Rosinstall installation
-sudo apt-get install python-rosinstall
+sudo apt-get install -y python-rosinstall
+
+# catkin
+sudo apt-get install -y python-catkin-tools
+
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> "$HOME/.bashrc"
+
+echo "Setting up basic workspace..."
+oldpwd="$PWD"
+mkdir -p $HOME/ros_ws/src
+cd $HOME/ros_ws/src/
+
+source "$HOME/.bashrc"
+catkin_init_workspace
+catkin build
+cd "$oldpwd"
+
+echo "source $HOME/ros_ws/devel/setup.bash" >> "$HOME/.bashrc"
+echo "Remember to source your bashrc to apply the changes..."
