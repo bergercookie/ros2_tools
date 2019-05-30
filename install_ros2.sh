f718ec59b53fccec594791e6591c1f148a7708cc
Nikos Koukis <nikolaos@slamcore.com>
2019-05-30 12:05:34 +0800

    Use apt-get instead of apt
    Also use `sudo -E` so that sudo inherits environment variables (handy to
    use with dockerfile ENV)

--- install_ros2.sh
+++ install_ros2.sh
@@ -4,10 +4,12 @@
 set -e
 
 # flags
-INSTALL_OPTIONAL=0
-USE_ROS1_PKGS=0  # additional packages for examples
-INSTALL_BASHRC_LINE=0
-INSTALL_EXTA_TOOLS=1  # for colcon etc.
+ROS_DISTRO="${ROS_DISTRO:-crystal}"
+ROS_DISTRO_VARIANT="${ROS_DISTRO_VARIANT:-desktop}"
+INSTALL_OPTIONAL="${INSTALL_OPTIONAL:-0}"
+USE_ROS1_PKGS="${USE_ROS1_PKGS:-0}"  # additional packages for examples
+INSTALL_BASHRC_LINE="${INSTALL_BASHRC_LINE:-0}"
+INSTALL_EXTA_TOOLS="${INSTALL_EXTRA_TOOLS:-0}"  # for colcon etc.
 
 function print_sec {
     printf "\n===============================================\n"
@@ -15,22 +17,11 @@ function print_sec {
     printf "===============================================\n\n"
 }
 
-# Determine ROS version to install
-ROS_DISTRO="crystal"  # default
-if [[ $# == "1" ]]
+if [[ "${ROS_DISTRO}" != "crystal" && "${ROS_DISTRO}" != "bouncy" && "${ROS_DISTRO}" != "ardent" ]]
 then
-    ROS_DISTRO="$1"
-    if [[ "${ROS_DISTRO}" != "crystal" && "${ROS_DISTRO}" != "bouncy" && "${ROS_DISTRO}" != "ardent" ]]
-    then
-        echo "[W] Unknown ROS distro provided [${ROS_DISTRO}]. Proceed cautiously...  <ENTER>"
-        read
-    fi
-elif [[ $# != "0" && $# != "1" ]]
-then
-    echo "Usage: "${BASH_SOURCE[0]}" [ROS-DISTRO-NAME]"
-    exit 1
+    echo "[W] Unknown ROS distro provided [${ROS_DISTRO}]. Proceed cautiously...  <ENTER>"
+    read
 fi
-export ROS_DISTRO
 
 print_sec "Checking system requirements"
 printf "\tlocale...\n"
@@ -53,16 +44,16 @@ fi
 
 
 print_sec "Installing bulk of packages"
-sudo apt update && sudo apt install curl gnupg2 lsb-release
-curl http://repo.ros2.org/repos.key | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update
-sudo apt install ros-${ROS_DISTRO}-desktop
-# sudo apt install ros-$ROS_DISTRO-ros-base
+sudo -E apt-get update && sudo -E apt-get install curl gnupg2 lsb-release
+curl http://repo.ros2.org/repos.key | sudo -E apt-key add -
+sudo -E sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo -E apt-get update
+sudo -E apt-get install "ros-${ROS_DISTRO}-${ROS_DISTRO_VARIANT}"
+# sudo -E apt-get install ros-$ROS_DISTRO-ros-base
 
 if [[ "$INSTALL_OPTIONAL" == "1" ]]
 then
-    sudo apt install python3-pip
+    sudo -E apt-get install python3-pip
     pip3 install --user argcomplete
 fi
 
@@ -76,14 +67,14 @@ fi
 if [[ "${ROS_DISTRO}" == "bouncy" ]]
 then
     print_sec "Installing additional RMW implementations"
-    sudo apt install ros-${ROS_DISTRO}-rmw-opensplice-cpp # for OpenSplice
-    sudo apt install ros-${ROS_DISTRO}-rmw-connext-cpp # for RTI Connext (requires license agreement)
+    sudo -E apt-get install ros-${ROS_DISTRO}-rmw-opensplice-cpp # for OpenSplice
+    sudo -E apt-get install ros-${ROS_DISTRO}-rmw-connext-cpp # for RTI Connext (requires license agreement)
 fi
 
 if [[ $USE_ROS1_PKGS == "1" ]]
 then
     print_sec "Installing ROS1 bridge"
-    sudo apt install ros-${ROS_DISTRO}-ros1-bridge
+    sudo -E apt-get install ros-${ROS_DISTRO}-ros1-bridge
 fi
 
 if [[ $USE_ROS1_PKGS == "1" ]]
@@ -91,7 +82,7 @@ then
     if [[ "${ROS_DISTRO}" != "crystal" ]]
     then
         print_sec "Installing turtlebot example2 packages"
-        sudo apt install ros-${ROS_DISTRO}-turtlebot2-*
+        sudo -E apt-get install ros-${ROS_DISTRO}-turtlebot2-*
     else
         printf "[W] turtlebot2 packages not available for crystal yet...\n"
     fi
@@ -100,13 +91,13 @@ fi
 if [[ $INSTALL_EXTA_TOOLS == "1" ]]
 then
     print_sec "Installing additional tools"
-    sudo apt install \
+    sudo -E apt-get install \
         python3-colcon-common-extensions \
         ros-${ROS_DISTRO}-tf2-*
 
     if [[ "${ROS_DISTRO}" != "ardent" ]]
     then
-    sudo apt install \
+    sudo -E apt-get install \
         ros-${ROS_DISTRO}-rqt-* \
         ros-${ROS_DISTRO}-image-transport \
         ros-${ROS_DISTRO}-rviz2*
